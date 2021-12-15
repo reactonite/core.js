edd7ff18d4e05538de26570e6586c03b00494e3d
anoushkanarang <anoushka19235@iiitd.ac.in>
2021-12-16 03:21:43 +0530

    Added documentation to CreateHtmlCss.js

--- commands/requirements/CreateHtmlCss.js
+++ commands/requirements/CreateHtmlCss.js
@@ -1,17 +1,12 @@
 var fs = require("fs");
 var path = require("path");
+/**
+ *Takes body of the POST response from GrapesJS and saves HTML File
 
+ * @param {string} content HTML Content
+ * @param {string} loc Location where we have to save the HTML File
+ */
 function saveHtml(content, loc) {
-  /*
-    Takes body of the POST response from GrapesJS and saves HTML File
-
-    Parameters
-    ----------
-    content : str
-        HTML Content
-    loc : str
-        Location where we have to save the HTML File
-*/
   var html_front =
     '<!doctype html><html lang="en"><head><meta charset="utf-8"/><title>The HTML5 Herald</title><meta name="description" content="The HTML5 Herald"/><meta name="author" content="SitePoint"/><link rel="stylesheet" href="./main.css"/></head><body>';
   var html_back = "</body></html>";
@@ -19,17 +14,12 @@ function saveHtml(content, loc) {
   fs.writeFileSync(path.join(loc, "index.html"), html_content, { flag: "w+" });
 }
 
+/**
+ *Takes body of the POST response from GrapesJS and saves CSS File
+ * @param {string} content CSS Content
+ * @param {location} loc Location where we have to save the CSS File
+ */
 function saveCss(content, loc) {
-  /*
-    Takes body of the POST response from GrapesJS and saves CSS File
-
-    Parameters
-    ----------
-    content : str
-        CSS Content
-    loc : str
-        Location where we have to save the CSS File
-    */
   fs.writeFileSync(path.join(loc, "main.css"), content, { flag: "w+" });
 }
 module.exports = {
