4bd5936421711804734330e81be0576ae66dbd12
anoushkanarang <anoushka19235@iiitd.ac.in>
2021-12-16 03:19:18 +0530

    Added documentation to Constants.js

--- commands/requirements/Constants.js
+++ commands/requirements/Constants.js
@@ -1,21 +1,14 @@
 const props_map = require("./PropsMap");
 class DEFAULTS {
-  /*Default variables used in code execution
-
-    Attributes
-    ----------
-    INIT_FILES_DIR : str
-        Directory for initial files for setup.
-    SRC_DIR : str
-        Source directory for reactonite codebase.
-    DEST_DIR : str
-        Destination directory for React codebase.
-    CONFIG_FILE_NAME : str
-        Config file name for config variables.
-    PROPS_MAP : dict
-        Mapping for HTML to React props
-    */
-
+  /**
+   * Default variables used in code execution
+   *
+   * @param {string} INIT_FILES_DIR Directory for initial files for setup.
+   * @param {string} SRC_DIR Source directory for reactonite codebase.
+   * @param {string} DEST_DIR Destination directory for React codebase.
+   * @param {string} CONFIG_FILE_NAME Config file name for config variables.
+   * @param {object} PROPS_MAP Mapping for HTML to React props
+   */
   constructor() {
     this.INIT_FILES_DIR = "init_src_dir";
     this.SRC_DIR = "src";
