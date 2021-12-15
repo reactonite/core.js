476f1a231fc2e5deb03deec21a4a56df6727ff8f
anoushkanarang <anoushka19235@iiitd.ac.in>
2021-12-16 03:16:25 +0530

    Added documentation to config.js

--- commands/requirements/Config.js
+++ commands/requirements/Config.js
@@ -2,25 +2,13 @@ const fs = require("fs");
 var { create_file, write_to_json_file } = require("./Helpers");
 
 class Config {
-  /*
-    A Class to manage and maintain project configuration.
-    One can add/remove/modify config variables and save/load
-    it easily
-
-    Attributes
-    ----------
-    config_path : str
-        Path to configuration file from where to load/save
-    config : dict
-        Config settings dictionary
-
-    Parameters
-    ----------
-    config_path : str
-        Path to configuration file from where to load/save
-    load : bool
-        Whether to load config on object creation, default is False
-  */
+  /**
+   * A Class to manage and maintain project configuration. One can add/remove/modify config variables and save/load it easily
+   *@property {string} config_path Path to configuration file from where to load/save
+   *@property {object} config Config settings dictionary
+   *@param {string} config_path Path to configuration file from where to load/save
+   *@param {boolean} load Whether to load config on object creation, default is false
+   */
 
   constructor(config_path, load = false) {
     this.config_path = config_path;
@@ -30,45 +18,48 @@ class Config {
       this.load_config();
     }
   }
-
+  /**
+   * Adds/updates a variable to the config
+   */
   add_to_config(config_name, config_value) {
-    // Adds/updates a variable to the config
     this.config[config_name] = config_value;
   }
 
+  /**
+   * Returns the configuration as a dictionary
+   */
   get_config() {
-    //Returns the configuration as a dictionary
     return this.config;
   }
 
+  /**
+   * Gets a config variable
+   */
   get(key) {
-    //Gets a config variable
     return this.config[key];
   }
 
-  save_config() {
-    /*
-    Saves the configuration to the config_path
+  /** 
+   * Saves the configuration to the config_path
     in JSoN format. It first creates the file if it
     doesn't exist, then writes into it.
-    */
-
+  */
+  save_config() {
     if (!fs.existsSync(this.config_path)) {
       create_file(this.config_path);
     }
     write_to_json_file(this.config_path, JSON.stringify(this.config));
   }
 
+  /**
+   * Loads the configuration from the config_path.
+   * @throws {FileNotFoundError} Error raised if config file not found at the config_path 
+        if not os.path.exists(self.config_path):
+        raise FileNotFoundError(
+            "Reactonite config.json file doesn't exist, can't proceed."
+        )
+  */
   load_config() {
-    /*
-    Loads the configuration from the config_path.
-
-    Raises
-    ------
-    FileNotFoundError
-        Raised if config file not found at the config_path 
-    */
-
     if (!fs.existsSync(this.config_path)) {
       throw new Error(
         "Reactonite config.json file doesn't exist, can't proceed."
@@ -77,8 +68,10 @@ class Config {
     this.config = JSON.parse(fs.readFileSync(this.config_path, "utf8"));
   }
 
+  /**
+   * Pretty prints the config variables.
+   */
   toString() {
-    //Pretty prints the config variables.
     return_str = "";
     for (const [key, value] of Object.entries(this.config)) {
       return_str += "{" + key + "}: {" + value + "}\n";
